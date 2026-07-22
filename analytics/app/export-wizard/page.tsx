5290eecf9a2f75e930eb882a17514a6d73377f9a
Tomike Tolulope <frontendpostmanreact@gmail.com>
2026-07-22 14:13:26 +0100

    feat: add export wizard page

---
+++ analytics/app/export-wizard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import ExportWizard from '@/analytics/components/ExportWizard'
+
+export default function ExportWizardPage() {
+  return (
+    <main style={{ minHeight: '100vh', background: '#f9fafb' }}>
+      <ExportWizard />
+    </main>
+  )
+}
